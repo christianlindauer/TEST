83e709e1e3d55bfa82d69d5c1e7901b1acf0a010
Hawner <dominik_good@hotmail.com>
2016-11-30 21:58:06 +0100

    implemented methods on IAutoRes.Service

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,58 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
+        // Car
+        [OperationContract]
+        List<AutoDto> getAllCars();
+
+        [OperationContract]
+        AutoDto getCarByPrimaryKey(int key);
+
+        [OperationContract]
+        void addCar(AutoDto car);
+
+        [OperationContract]
+        void updateCar(AutoDto car);
+
+        [OperationContract]
+        void deleteCar(AutoDto car);
+
+        // Customer
+        [OperationContract]
+        List<KundeDto> getAllCustomers();
+
+        [OperationContract]
+        KundeDto getCustomerByPrimaryKey(int key);
+
+        [OperationContract]
+        void addCustomer(KundeDto car);
+
+        [OperationContract]
+        void updateCustomer(KundeDto car);
+
+        [OperationContract]
+        void deleteCustomer(KundeDto car);
+
+        // Reservation
+        [OperationContract]
+        List<ReservationDto> getAllReservations();
+
+        [OperationContract]
+        ReservationDto getReservationByPrimaryKey(int key);
+
+        [OperationContract]
+        void addReservation(ReservationDto car);
+
+        [OperationContract]
+        void updateReservation(ReservationDto car);
+
+        [OperationContract]
+        void deleteReservation(ReservationDto car);
     }
 }
