0e526f3ff3b11a04ddf93263311ab71e7c9c6c6c
Hawner <dominik_good@hotmail.com>
2016-12-01 11:33:41 +0100

    Buis.Layer Method Bodies added

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -20,5 +20,38 @@ namespace AutoReservation.BusinessLayer
 
             return new LocalOptimisticConcurrencyException<T>($"Update {typeof(Auto).Name}: Concurrency-Fehler", dbEntity);
         }
+
+
+        // Car
+        public List<Auto> getAllCars() { return null; }
+        public Auto getCarByPrimaryKey(int key) { return null; }
+
+        public void addCar(Auto car) { }
+
+        public void updateCar(Auto car) { }
+
+        public void deleteCar(Auto car) { }
+
+        // Customer
+        public List<Kunde> getAllCustomers() { return null; }
+
+        public Kunde getCustomerByPrimaryKey(int key) { return null; }
+
+        public void addCustomer(Kunde customer) { }
+
+        public void updateCustomer(Kunde customer) { }
+
+        public void deleteCustomer(Kunde customer) { }
+
+        // Reservation
+        public List<Reservation> getAllReservations() { return null; }
+
+        public Reservation getReservationByPrimaryKey(int key) { return null; }
+
+        public void addReservation(Reservation reservation) { }
+
+        public void updateReservation(Reservation reservation) { }
+
+        public void deleteReservation(Reservation reservation) { }
     }
 }
