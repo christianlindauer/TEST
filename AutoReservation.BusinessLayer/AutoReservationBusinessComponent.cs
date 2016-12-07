0a0fadb001b533c69262af47ccd110b611a4af36
Hawner <dominik_good@hotmail.com>
2016-12-07 16:37:24 +0100

    CRUD for Custo & Res, Implemented Tests on Buisiness Layer

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -82,25 +82,129 @@ namespace AutoReservation.BusinessLayer
         }
 
         // Customer
-        public List<Kunde> getAllCustomers() { return null; }
+        public List<Kunde> getAllCustomers() {
+            List<Kunde> listOfAllCustomers = new List<Kunde>();
+            using (var db = new AutoReservationContext())
+            {
+                var query = from Kunde kunde in db.Kunden
+                            select kunde;
+                foreach (Kunde customer in query)
+                {
+                    listOfAllCustomers.Add(customer);
+                }
+                return listOfAllCustomers;
+            }
+        }
 
-        public Kunde getCustomerByPrimaryKey(int key) { return null; }
+        public Kunde getCustomerByPrimaryKey(int key) {
+            using (var db = new AutoReservationContext())
+            {
+                var query = from Kunde kunde in db.Kunden
+                            where kunde.Id == key
+                            select kunde;
 
-        public void addCustomer(Kunde customer) { }
+                foreach (Kunde customer in query)
+                {
+                    return customer;
+                }
+                return null;
+            }
+        }
 
-        public void updateCustomer(Kunde customer) { }
+        public void addCustomer(Kunde customer) {
+            using (var db = new AutoReservationContext())
+            {
+                db.Entry(customer).State = EntityState.Added;
+                db.Kunden.Add(customer);
+                db.SaveChanges();
+            }
+        }
 
-        public void deleteCustomer(Kunde customer) { }
+        public void updateCustomer(Kunde customer) {
+            using (var db = new AutoReservationContext())
+            {
+                try
+                {
+                    db.Entry(customer).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw CreateLocalOptimisticConcurrencyException(db, customer);
+                }
+            }
+        }
+
+        public void deleteCustomer(Kunde customer) {
+            using (var db = new AutoReservationContext())
+            {
+                db.Entry(customer).State = EntityState.Deleted;
+                db.Kunden.Remove(customer);
+                db.SaveChanges();
+            }
+        }
 
         // Reservation
-        public List<Reservation> getAllReservations() { return null; }
+        public List<Reservation> getAllReservations() {
+            List<Reservation> listOfReservations = new List<Reservation>();
+            using (var db = new AutoReservationContext())
+            {
+                var query = from Reservation res in db.Reservationen
+                            select res;
+                foreach (Reservation res in query)
+                {
+                    listOfReservations.Add(res);
+                }
+                return listOfReservations;
+            }
+        }
 
-        public Reservation getReservationByPrimaryKey(int key) { return null; }
+        public Reservation getReservationByPrimaryKey(int key) {
+            using (var db = new AutoReservationContext())
+            {
+                var query = from Reservation res in db.Reservationen
+                            where res.ReservationsNr == key
+                            select res;
 
-        public void addReservation(Reservation reservation) { }
+                foreach (Reservation res in query)
+                {
+                    return res;
+                }
+                return null;
+            }
+        }
 
-        public void updateReservation(Reservation reservation) { }
+        public void addReservation(Reservation reservation) {
+            using (var db = new AutoReservationContext())
+            {
+                db.Entry(reservation).State = EntityState.Added;
+                db.Reservationen.Add(reservation);
+                db.SaveChanges();
+            }
+        }
 
-        public void deleteReservation(Reservation reservation) { }
+        public void updateReservation(Reservation reservation) {
+            using (var db = new AutoReservationContext())
+            {
+                try
+                {
+                    db.Entry(reservation).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    throw CreateLocalOptimisticConcurrencyException(db, reservation);
+                }
+            }
+        }
+
+        public void deleteReservation(Reservation reservation) {
+            using (var db = new AutoReservationContext())
+            {
+                db.Entry(reservation).State = EntityState.Deleted;
+                db.Reservationen.Remove(reservation);
+                db.SaveChanges();
+            }
+        }
     }
 }
