6f6c86e3f7d448d6575fe53ecf3654235331dce7
Hawner <dominik_good@hotmail.com>
2016-12-07 16:52:38 +0100

    DTO's Kunde & Reservation implementiert

--- AutoReservation.Common/DataTransferObjects/KundeDto.cs
+++ AutoReservation.Common/DataTransferObjects/KundeDto.cs
@@ -1,33 +1,115 @@
 ﻿using AutoReservation.Common.DataTransferObjects.Core;
+using System;
+using System.Runtime.Serialization;
+using System.Text;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
-    public class KundeDto //: DtoBase<KundeDto>
+    [DataContract]
+    public class KundeDto : DtoBase<KundeDto>
     {
+        private DateTime geburtsdatum;
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (string.IsNullOrEmpty(Nachname))
-        //    {
-        //        error.AppendLine("- Nachname ist nicht gesetzt.");
-        //    }
-        //    if (string.IsNullOrEmpty(Vorname))
-        //    {
-        //        error.AppendLine("- Vorname ist nicht gesetzt.");
-        //    }
-        //    if (Geburtsdatum == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Geburtsdatum ist nicht gesetzt.");
-        //    }
+        [DataMember]
+        public DateTime Geburtsdatum
+        {
+            get { return geburtsdatum; }
+            set {
+                if (geburtsdatum != value)
+                {
+                    geburtsdatum = value;
+                    OnPropertyChanged(nameof(Geburtsdatum));
+                }
+            }
+        }
 
-        //    if (error.Length == 0) { return null; }
+        private int id;
 
-        //    return error.ToString();
-        //}
+        [DataMember]
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
 
-        //public override string ToString()
-        //    => $"{Id}; {Nachname}; {Vorname}; {Geburtsdatum}";
+        private string nachname;
+
+        [DataMember]
+        public string Nachname
+        {
+            get { return nachname; }
+            set
+            {
+                if (nachname != value)
+                {
+                    nachname = value;
+                    OnPropertyChanged(nameof(Nachname));
+                }
+            }
+        }
+
+        private byte[] rowVersion;
+
+        [DataMember]
+        public byte[] RowVersion
+        {
+            get { return rowVersion; }
+            set
+            {
+                if (rowVersion != value)
+                {
+                    rowVersion = value;
+                    OnPropertyChanged(nameof(RowVersion));
+                }
+            }
+        }
+
+        private string vorname;
+
+        [DataMember]
+        public string Vorname
+        {
+            get { return vorname; }
+            set
+            {
+                if (vorname != value)
+                {
+                    vorname = value;
+                    OnPropertyChanged(nameof(Vorname));
+                }
+            }
+        }
+
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrEmpty(Nachname))
+            {
+                error.AppendLine("- Nachname ist nicht gesetzt.");
+            }
+            if (string.IsNullOrEmpty(Vorname))
+            {
+                error.AppendLine("- Vorname ist nicht gesetzt.");
+            }
+            if (Geburtsdatum == DateTime.MinValue)
+            {
+                error.AppendLine("- Geburtsdatum ist nicht gesetzt.");
+            }
+
+            if (error.Length == 0) { return null; }
+
+            return error.ToString();
+        }
+
+        public override string ToString()
+            => $"{Id}; {Nachname}; {Vorname}; {Geburtsdatum}";
 
     }
 }
--- AutoReservation.Common/DataTransferObjects/ReservationDto.cs
+++ AutoReservation.Common/DataTransferObjects/ReservationDto.cs
@@ -1,57 +1,188 @@
 ﻿using AutoReservation.Common.DataTransferObjects.Core;
+using System.Runtime.Serialization;
+using System.Text;
+using System;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
-    public class ReservationDto //: DtoBase<ReservationDto>
+    [DataContract]
+    public class ReservationDto : DtoBase<ReservationDto>
     {
+        private int autoId;
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (Von == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Von-Datum ist nicht gesetzt.");
-        //    }
-        //    if (Bis == DateTime.MinValue)
-        //    {
-        //        error.AppendLine("- Bis-Datum ist nicht gesetzt.");
-        //    }
-        //    if (Von > Bis)
-        //    {
-        //        error.AppendLine("- Von-Datum ist grösser als Bis-Datum.");
-        //    }
-        //    if (Auto == null)
-        //    {
-        //        error.AppendLine("- Auto ist nicht zugewiesen.");
-        //    }
-        //    else
-        //    {
-        //        string autoError = Auto.Validate();
-        //        if (!string.IsNullOrEmpty(autoError))
-        //        {
-        //            error.AppendLine(autoError);
-        //        }
-        //    }
-        //    if (Kunde == null)
-        //    {
-        //        error.AppendLine("- Kunde ist nicht zugewiesen.");
-        //    }
-        //    else
-        //    {
-        //        string kundeError = Kunde.Validate();
-        //        if (!string.IsNullOrEmpty(kundeError))
-        //        {
-        //            error.AppendLine(kundeError);
-        //        }
-        //    }
+        [DataMember]
+        public int AutoId
+        {
+            get { return autoId; }
+            set
+            {
+                if (autoId != value)
+                {
+                    autoId = value;
+                    OnPropertyChanged(nameof(AutoId));
+                }
+            }
+        }
 
-        //    if (error.Length == 0) { return null; }
+        private DateTime bis;
 
-        //    return error.ToString();
-        //}
+        [DataMember]
+        public DateTime Bis
+        {
+            get { return bis; }
+            set
+            {
+                if (bis != value)
+                {
+                    bis = value;
+                    OnPropertyChanged(nameof(Bis));
+                }
+            }
+        }
 
-        //public override string ToString()
-        //    => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
+        private int kundeId;
+
+        [DataMember]
+        public int KundeId
+        {
+            get { return kundeId; }
+            set
+            {
+                if (kundeId != value)
+                {
+                    kundeId = value;
+                    OnPropertyChanged(nameof(KundeId));
+                }
+            }
+        }
+
+        private int reservationsNr;
+
+        [DataMember]
+        public int ReservationsNr
+        {
+            get { return reservationsNr; }
+            set
+            {
+                if (reservationsNr != value)
+                {
+                    reservationsNr = value;
+                    OnPropertyChanged(nameof(ReservationsNr));
+                }
+            }
+        }
+
+        private byte[] rowVersion;
+
+        [DataMember]
+        public byte[] RowVersion
+        {
+            get { return rowVersion; }
+            set
+            {
+                if (rowVersion != value)
+                {
+                    rowVersion = value;
+                    OnPropertyChanged(nameof(RowVersion));
+                }
+            }
+        }
+
+        private DateTime von;
+
+        [DataMember]
+        public DateTime Von
+        {
+            get { return von; }
+            set
+            {
+                if (von != value)
+                {
+                    von = value;
+                    OnPropertyChanged(nameof(Von));
+                }
+            }
+        }
+
+        private KundeDto kunde;
+
+        [DataMember]
+        public KundeDto Kunde
+        {
+            get { return kunde; }
+            set
+            {
+                if (kunde != value)
+                {
+                    kunde = value;
+                    OnPropertyChanged(nameof(Kunde));
+                }
+            }
+        }
+
+        private AutoDto auto;
+
+        [DataMember]
+        public AutoDto Auto
+        {
+            get { return auto; }
+            set
+            {
+                if (auto != value)
+                {
+                    auto = value;
+                    OnPropertyChanged(nameof(Auto));
+                }
+            }
+        }
+
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (Von == DateTime.MinValue)
+            {
+                error.AppendLine("- Von-Datum ist nicht gesetzt.");
+            }
+            if (Bis == DateTime.MinValue)
+            {
+                error.AppendLine("- Bis-Datum ist nicht gesetzt.");
+            }
+            if (Von > Bis)
+            {
+                error.AppendLine("- Von-Datum ist grösser als Bis-Datum.");
+            }
+            if (Auto == null)
+            {
+                error.AppendLine("- Auto ist nicht zugewiesen.");
+            }
+            else
+            {
+                string autoError = Auto.Validate();
+                if (!string.IsNullOrEmpty(autoError))
+                {
+                    error.AppendLine(autoError);
+                }
+            }
+            if (Kunde == null)
+            {
+                error.AppendLine("- Kunde ist nicht zugewiesen.");
+            }
+            else
+            {
+                string kundeError = Kunde.Validate();
+                if (!string.IsNullOrEmpty(kundeError))
+                {
+                    error.AppendLine(kundeError);
+                }
+            }
+
+            if (error.Length == 0) { return null; }
+
+            return error.ToString();
+        }
+
+        public override string ToString()
+            => $"{ReservationsNr}; {Von}; {Bis}; {Auto}; {Kunde}";
 
     }
 }
